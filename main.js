60d47cecf56f7506e3cb1de3710f79e724c1f2b4
--Explosion-- <explosion.scratch@gmail.com>
2020-10-05 15:16:45 +0000

    It should include jQuery now!

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
-
+include('https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js');
 function include(file) { 
   
   var script  = document.createElement('script'); 
