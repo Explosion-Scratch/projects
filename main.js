10e33f9ca511415e0ac2d88eeb2d957a5015f705
--Explosion-- <explosion.scratch@gmail.com>
2020-10-05 15:16:12 +0000

    Include

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert('text');
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+
+function include(file) { 
+  
+  var script  = document.createElement('script'); 
+  script.src  = file; 
+  script.type = 'text/javascript'; 
+  script.defer = true; 
+  
+  document.getElementsByTagName('head').item(0).appendChild(script); 
+  
+} 
