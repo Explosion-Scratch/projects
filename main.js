0433db3af469b84717516eb4a6bb6745e6950161
--Explosion-- <explosion.scratch@gmail.com>
2020-10-07 23:47:04 +0000

    ;

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function insertHR(element) {
 	var articles = document.getElementsByTagName(element)
 		for (var i = 0; i < articles.length; i ++) {
-				articles[i].parentNode.insertBefore(document.createElement('hr'), articles[i].nextSibling)
-		}
-}
-window.onload = insertHR('h1')
+				articles[i].parentNode.insertBefore(document.createElement('hr'), articles[i].nextSibling);
+		};
+};
+window.onload = insertHR('h1');
+window.onload = insertHR('h2');
+window.onload = insertHR('h1');
