fd173e5d08a7caada70380564f9746ccd359e884
--Explosion-- <explosion.scratch@gmail.com>
2020-10-07 23:50:55 +0000

    Stupid thing.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+document.onload = insertHR('h1');
+document.onload = insertHR('h2');
+document.onload = insertHR('h1');
+document.onload = alert('hi');
+
 function insertHR(element) {
 	var articles = document.getElementsByTagName(element)
 		for (var i = 0; i < articles.length; i ++) {
 				articles[i].parentNode.insertBefore(document.createElement('hr'), articles[i].nextSibling);
 		};
 };
-document.onload = insertHR('h1');
-document.onload = insertHR('h2');
-document.onload = insertHR('h1');
-document.onload = alert('hi');
